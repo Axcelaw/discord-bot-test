140c2805dafa90150487a0c952ae573ca9fa3457
Axcelaw <walace.andr.m@gmail.com>
2023-01-11 14:06:44 -0300

    Bot gets the selected menu choice and returns it.

--- commands/select.js
+++ commands/select.js
@@ -1,35 +1,43 @@
-const { ActionRowBuilder, StringSelectMenuBuilder, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-
-const row = new ActionRowBuilder()
-  .addComponents(
-    new StringSelectMenuBuilder()
-      .setCustomId('select')
-      .setPlaceholder('Nothing selected')
-      .addOptions(
-        {
-          label: 'Select me',
-          description: 'This is a description',
-          value: 'first_option',
-        },
-        {
-          label: 'You can select me too',
-          description: 'This is also a description',
-          value: 'second_option',
-        },
-      ),
-  );
-
-const embed = new EmbedBuilder()
-  .setColor(0x0099FF)
-  .setTitle('Some title')
-  .setURL('https://discord.js.org/')
-  .setDescription('Some description here');
+const { ActionRowBuilder, StringSelectMenuBuilder, SlashCommandBuilder, EmbedBuilder, ComponentType } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
 		.setName('select')
 		.setDescription('Renders a select menu.'),
   async execute(interaction) {
-    await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] });
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new StringSelectMenuBuilder()
+          .setCustomId('select')
+          .setPlaceholder('Nothing selected')
+          .addOptions(
+            {
+              label: 'Select me',
+              description: 'This is a description',
+              value: 'first_option',
+            },
+            {
+              label: 'You can select me too',
+              description: 'This is also a description',
+              value: 'second_option',
+            },
+          ),
+      );
+    
+    const message = await interaction.reply({ content: 'Pong!', ephemeral: true, components: [row] });
+
+    const collector = message.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 15000 });
+
+    collector.on('collect', i => {
+      if (i.user.id === interaction.user.id) {
+        i.reply(`${i.user} clicked on ${i.customId}: ${i.values[0]}`);
+      } else {
+        i.reply({ content: `These buttons aren't for you!`, ephemeral: true });
+      }
+    });
+
+    collector.on('end', collected => {
+      console.log(`Collected ${collected.size} interactions.`);
+    });
   }
 };
